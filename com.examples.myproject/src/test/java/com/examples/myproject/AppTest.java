08eb55eaf72d5960b3bbf322e0f73c9288e49e03
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2016-11-08 15:03:47 +0100

    initial commit

---
+++ com.examples.myproject/src/test/java/com/examples/myproject/AppTest.java
@@ -0,0 +1,25 @@
+package com.examples.myproject;
+
+/**
+ * Unit test for simple App.
+ */
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	private App app;
+
+	@Before
+	public void setup() {
+		app = new App();
+	}
+
+	@org.junit.Test
+	public void testSayHello() {
+		assertEquals("Hello", app.sayHello());
+	}
+}
